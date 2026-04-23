22bb9738adf19ceca81fab3614a5a1710b17565a
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-22 19:51:05 -0500

    Various fixes

--- test/Soenneker.Utils.Strings.HammingDistance.Tests/HammingDistanceStringUtilTests.cs
+++ test/Soenneker.Utils.Strings.HammingDistance.Tests/HammingDistanceStringUtilTests.cs
@@ -12,14 +12,14 @@ public class HammingDistanceStringUtilTests : HostedUnitTest
     {
     }
 
-    [Theory]
-    [InlineData("", "", 100.0)] // Empty strings are identical
-    [InlineData("a", "b", 0.0)] // Completely different single characters
-    [InlineData("abc", "def", 0.0)] // Completely different strings
-    [InlineData("abc", "abc", 100.0)] // Identical strings
-    [InlineData("1010", "1111", 50.0)] // Two differences out of four
-    [InlineData("kitten", "sitten", (5.0 / 6.0) * 100)] // One difference out of six
-    [InlineData("abcdef", "abcxyz", 50.0)] // Half are different
+    [Test]
+    [Arguments("", "", 100.0)] // Empty strings are identical
+    [Arguments("a", "b", 0.0)] // Completely different single characters
+    [Arguments("abc", "def", 0.0)] // Completely different strings
+    [Arguments("abc", "abc", 100.0)] // Identical strings
+    [Arguments("1010", "1111", 50.0)] // Two differences out of four
+    [Arguments("kitten", "sitten", (5.0 / 6.0) * 100)] // One difference out of six
+    [Arguments("abcdef", "abcxyz", 50.0)] // Half are different
     public void CalculateSimilarityPercentage_Returns_Correct_Percentage(string str1, string str2, double expectedPercentage)
     {
         double similarityPercentage = HammingDistanceStringUtil.CalculatePercentage(str1, str2);
@@ -27,9 +27,9 @@ public class HammingDistanceStringUtilTests : HostedUnitTest
         similarityPercentage.Should().BeApproximately(expectedPercentage, 0.001);
     }
 
-    [Theory]
-    [InlineData("abc", "abcd")] // Unequal length, should throw exception
-    [InlineData("", "a")] // Unequal length, should throw exception
+    [Test]
+    [Arguments("abc", "abcd")] // Unequal length, should throw exception
+    [Arguments("", "a")] // Unequal length, should throw exception
     public void CalculateSimilarityPercentage_Throws_ArgumentException_For_Unequal_Lengths(string str1, string str2)
     {
         Action act = () => HammingDistanceStringUtil.CalculatePercentage(str1, str2);
@@ -37,14 +37,14 @@ public class HammingDistanceStringUtilTests : HostedUnitTest
         act.Should().Throw<ArgumentException>().WithMessage("Strings must be of equal length*");
     }
 
-    [Theory]
-    [InlineData("", "", 1.0)] // Empty strings are identical
-    [InlineData("a", "b", 0.0)] // Completely different single characters
-    [InlineData("abc", "def", 0.0)] // Completely different strings
-    [InlineData("abc", "abc", 1.0)] // Identical strings
-    [InlineData("1010", "1111", 0.5)] // Two differences out of four
-    [InlineData("kitten", "sitten", 5.0 / 6.0)] // One difference out of six
-    [InlineData("abcdef", "abcxyz", 0.5)] // Half are different
+    [Test]
+    [Arguments("", "", 1.0)] // Empty strings are identical
+    [Arguments("a", "b", 0.0)] // Completely different single characters
+    [Arguments("abc", "def", 0.0)] // Completely different strings
+    [Arguments("abc", "abc", 1.0)] // Identical strings
+    [Arguments("1010", "1111", 0.5)] // Two differences out of four
+    [Arguments("kitten", "sitten", 5.0 / 6.0)] // One difference out of six
+    [Arguments("abcdef", "abcxyz", 0.5)] // Half are different
     public void CalculateSimilarity_Returns_Correct_Similarity_Score(string str1, string str2, double expectedScore)
     {
         double similarityScore = HammingDistanceStringUtil.Calculate(str1, str2);
@@ -52,9 +52,9 @@ public class HammingDistanceStringUtilTests : HostedUnitTest
         similarityScore.Should().BeApproximately(expectedScore, 0.001);
     }
 
-    [Theory]
-    [InlineData("abc", "abcd")] // Unequal length, should throw exception
-    [InlineData("", "a")] // Unequal length, should throw exception
+    [Test]
+    [Arguments("abc", "abcd")] // Unequal length, should throw exception
+    [Arguments("", "a")] // Unequal length, should throw exception
     public void CalculateSimilarity_Throws_ArgumentException_For_Unequal_Lengths(string str1, string str2)
     {
         Action act = () => HammingDistanceStringUtil.Calculate(str1, str2);
@@ -63,3 +63,4 @@ public class HammingDistanceStringUtilTests : HostedUnitTest
     }
 
 }
+
